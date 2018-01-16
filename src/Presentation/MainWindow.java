50a78de7cbfca77f9f17fb119c575c5ed7f424a1
Ferin <kevin.ferin.3@gmail.com>
2018-01-16 11:27:54 +0100

    fenetre
    fenetre

--- src/Presentation/MainWindow.java
+++ src/Presentation/MainWindow.java
@@ -1,5 +1,98 @@
 package Presentation;
 
-public class MainWindow {
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class MainWindow extends JFrame{
+	
+	public MainWindow () { 
+		super("ESAT"); 
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		Container c = this.getContentPane(); 
+		c.setLayout(new GridLayout(2,1));
+		JPanel choix = new JPanel(); 
+		choix.setLayout(new GridLayout(2,13));
+		
+		JCheckBox check = new JCheckBox("Tri"); 
+		Integer[] prog = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24}; 
+		JComboBox<Integer> lavage = new JComboBox<Integer>(prog);
+		JLabel NomProg = new JLabel("Programme");
+		JPanel programme = new JPanel(); 
+		programme.setLayout(new BorderLayout());
+		programme.add(NomProg,BorderLayout.WEST);
+		programme.add(lavage,BorderLayout.CENTER);
+		
+		Integer[] mach = {1,2,3,4,5}; 
+		JComboBox<Integer> machine = new JComboBox<Integer>(mach);
+		JLabel NomMachine = new JLabel("Machine");
+		JPanel mac = new JPanel(); 
+		mac.setLayout(new BorderLayout());
+		mac.add(NomMachine,BorderLayout.WEST);
+		mac.add(machine,BorderLayout.CENTER);
+		
+		JCheckBox presech = new JCheckBox("Préséchage"); 
+		
+		Integer[] sech = {0,1,2,3}; 
+		JComboBox<Integer> sechoir = new JComboBox<Integer>(sech);
+		JLabel NomSech = new JLabel("Sechage");
+		JPanel sec = new JPanel(); 
+		sec.setLayout(new BorderLayout());
+		sec.add(NomSech,BorderLayout.WEST);
+		sec.add(sechoir,BorderLayout.CENTER);
+		
+		JCheckBox calandre = new JCheckBox("Calandre"); 
+		
+		JCheckBox defroissage = new JCheckBox("Défroissage"); 
+		
+		JCheckBox repassage = new JCheckBox("Repassage"); 
+		
+		JCheckBox cintre = new JCheckBox("Mise sur cintre"); 
+		
+		JCheckBox pliage = new JCheckBox("Pliage"); 
+		
+		JCheckBox emballage = new JCheckBox("Emballage"); 
+		
+		
+		
+		choix.add(check);
+		choix.add(mac); 
+		choix.add(programme);
+		choix.add(presech);
+		choix.add(sec);
+		choix.add(calandre);
+		choix.add(defroissage);
+		choix.add(repassage);
+		choix.add(cintre);
+		choix.add(pliage);
+		choix.add(emballage);
+		
+		c.add(choix); 
+		JPanel resul = new JPanel();
+		resul.setLayout(new GridLayout(1,2));
+		
+		JButton calculer = new JButton("Calcul");
+		resul.add(calculer); 
+		JLabel resultat = new JLabel("bite"); 
+		resultat.setHorizontalAlignment(0);
+		resul.add(resultat); 
+		
+		c.add(resul); 
+		this.pack(); 
+	}
+	
+	public static void main (String args[]) {
+		MainWindow f = new MainWindow(); 
+		
+		f.setVisible(true);
+		
+	}
 }
