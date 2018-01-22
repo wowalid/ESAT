bc35d437d9c3ef5e00736f8d3170535acc98d172
Walid <walidarbel77@gmail.com>
2018-01-22 09:28:58 +0100

    Excel exporter

--- src/Presentation/fenetreModifier.java
+++ src/Presentation/fenetreModifier.java
@@ -22,36 +22,39 @@ public class fenetreModifier extends JFrame{
 		c.setLayout(new GridLayout(4,1));
 		
 		JPanel elec = new JPanel();
-		elec.setLayout(new GridLayout(1,2));
+		elec.setLayout(new GridLayout(1,3));
 		JLabel nomElec = new JLabel("Prix élécricité en kWh");
 		JTextField newElec = new JTextField() ;
 		elec.add(nomElec);
 		elec.add(newElec);
-		
+		JButton modifierElec = new JButton("Modifier");
+		elec.addActionListener(new controlEnregistrer(this,newElec,this.donnees));
+		elec.add(modifierElec);
 		
 		JPanel eau = new JPanel();
-		eau.setLayout(new GridLayout(1,2));
+		eau.setLayout(new GridLayout(1,3));
 		JLabel nomEau = new JLabel("Prix de l'eau en m3"); 
 		JTextField newEau = new JTextField() ;
 		eau.add(nomEau);
 		eau.add(newEau);
-		
+		JButton modifierEau = new JButton("Modifier");
+		eau.addActionListener(new controlEnregistrer(this,newEau, this.donnees));
+		eau.add(modifierEau);
 		
 		JPanel gaz = new JPanel(); 
-		gaz.setLayout(new GridLayout(1,2));
+		gaz.setLayout(new GridLayout(1,3));
 		JLabel nomGaz = new JLabel("Prix du gaz en kWh"); 
 		JTextField newGaz = new JTextField() ;
 		gaz.add(nomGaz);
 		gaz.add(newGaz);
+		JButton modifierGaz = new JButton("Modifier");
+		gaz.addActionListener(new controlEnregistrer(this, newGaz,this.donnees));
+		gaz.add(modifierGaz);
 		
 		c.add(elec);
 		c.add(eau);
 		c.add(gaz);
-		
-		JButton enregistrer = new JButton("Enregistrer"); 
-		enregistrer.addActionListener(new controlEnregistrer(this,newElec, newEau, newGaz,this.donnees));
-		
-		c.add(enregistrer); 
+ 
 		this.pack(); 
 		
 	}
