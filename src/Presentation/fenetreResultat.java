7b7a467645016ee6f7303bacb2e4d0f9fa99da81
Ferin <kevin.ferin.3@gmail.com>
2018-01-25 22:08:36 +0100

    suce ma bite

--- src/Presentation/fenetreResultat.java
+++ src/Presentation/fenetreResultat.java
@@ -29,7 +29,7 @@ public class fenetreResultat extends JFrame{
 		m1.setLayout(new GridLayout(1,2));
 		JLabel mach1= new JLabel("Machine 1");
 		JLabel prix1 = new JLabel ("1") ;
-		this.donnees.addObserver(new controlLabel(0,this.donnees.ResultatParMachine,prix1));
+		this.donnees.addObserver(new controlLabel(0,this.donnees.ResultatParMachine,prix1,this.donnees,mach1));
 		m1.add(mach1);
 		m1.add(prix1);
 		
@@ -37,7 +37,7 @@ public class fenetreResultat extends JFrame{
 		m2.setLayout(new GridLayout(1,2));
 		JLabel mach2= new JLabel("Machine 2");
 		JLabel prix2 = new JLabel ("2") ;
-		this.donnees.addObserver(new controlLabel(1,this.donnees.ResultatParMachine,prix2));
+		this.donnees.addObserver(new controlLabel(1,this.donnees.ResultatParMachine,prix2,this.donnees,mach2));
 		m2.add(mach2);
 		m2.add(prix2);
 		
@@ -45,7 +45,7 @@ public class fenetreResultat extends JFrame{
 		m3.setLayout(new GridLayout(1,2));
 		JLabel mach3= new JLabel("Machine 3");
 		JLabel prix3 = new JLabel ("3") ;
-		this.donnees.addObserver(new controlLabel(2,this.donnees.ResultatParMachine,prix3));
+		this.donnees.addObserver(new controlLabel(2,this.donnees.ResultatParMachine,prix3,this.donnees,mach3));
 		m3.add(mach3);
 		m3.add(prix3);
 		
@@ -53,7 +53,7 @@ public class fenetreResultat extends JFrame{
 		m4.setLayout(new GridLayout(1,2));
 		JLabel mach4= new JLabel("Machine 4");
 		JLabel prix4 = new JLabel ("4") ;
-		this.donnees.addObserver(new controlLabel(3,this.donnees.ResultatParMachine,prix4));
+		this.donnees.addObserver(new controlLabel(3,this.donnees.ResultatParMachine,prix4,this.donnees,mach4));
 		m4.add(mach4);
 		m4.add(prix4);
 		
@@ -61,7 +61,7 @@ public class fenetreResultat extends JFrame{
 		m5.setLayout(new GridLayout(1,2));
 		JLabel mach5= new JLabel("Machine 5");
 		JLabel prix5 = new JLabel ("5") ;
-		this.donnees.addObserver(new controlLabel(4,this.donnees.ResultatParMachine,prix5));
+		this.donnees.addObserver(new controlLabel(4,this.donnees.ResultatParMachine,prix5,this.donnees,mach5));
 		m5.add(mach5);
 		m5.add(prix5);
 		
--- src/control/controlLabel.java
+++ src/control/controlLabel.java
@@ -5,20 +5,41 @@ import java.util.Observer;
 
 import javax.swing.JLabel;
 
+import Abstraction.Donnees;
+
 public class controlLabel implements Observer{
 	public int machine; 
 	public double[] resultatParMachine; 
 	public JLabel label ; 
+	public Donnees donnees ; 
+	public JLabel mach; 
 	
-	public controlLabel (int machine,double[] resultatParMachine,JLabel label) { 
+	public controlLabel (int machine,double[] resultatParMachine,JLabel label, Donnees donnees,JLabel mach) { 
 		this.machine=machine; 
 		this.resultatParMachine=resultatParMachine; 
 		this.label = label; 
+		this.donnees=donnees; 
+		this.mach=mach;
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
-		this.label.setText(""+resultatParMachine[machine]);
+		if ((this.mach.getText().equals("Machine 2"))||(this.mach.getText().equals("Machine 5"))){
+			if (this.donnees.boutonValeurs[1]<=10) {
+				this.label.setText(""+resultatParMachine[machine]);
+			}
+			else { 
+				this.label.setText("");
+			}
+		}
+		else { 
+			if (this.donnees.boutonValeurs[1]>=10) {
+				this.label.setText(""+resultatParMachine[machine]);
+			}
+			else { 
+				this.label.setText("");
+			}
+		}
 		
 	}
 
